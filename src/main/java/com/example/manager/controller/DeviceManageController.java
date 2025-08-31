ec8512d890fab2229330b0b6b309eb3f3cd9ca3f
aotenjou <hubert20041222@126.com>
2025-08-31 16:43:25 +0800

    TODO:guidance/security .

--- src/main/java/com/example/manager/controller/DeviceManageController.java
+++ src/main/java/com/example/manager/controller/DeviceManageController.java
@@ -82,7 +82,7 @@ public class DeviceManageController {
         boolean success = deviceService.updateDevice(device);
         if (success) {
             response.put("status", "success");
-            response.put("message", "更新成功");
+            response.put("message", "设备信息更新成功");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", "error");
